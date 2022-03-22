506cca7eec3fc742ee509442669c1c425ac8ec3e
Tanya <tanaybrown@gmail.com>
2022-03-21 20:55:36 -0700

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+const cards = []
+
+function writeCards(name, eventName) {
+   
+    for(let i = 0; i < name.length; i++) {
+       
+        cards.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+        
+    }
+    return cards
+}
+
+
+
+const countDownArray = []
+
+function countDown() {
+    let i = 10
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
